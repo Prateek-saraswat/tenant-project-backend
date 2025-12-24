d0918a25124fabf4b8c170d323e5ec44eec5be26
Prateek-saraswat <saraswatprateek0@gmail.com>
2025-12-24 15:54:47 +0530

    cors updated

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
+
 const app = express();
 
 // Security Middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
+    origin: true,
+  credentials: true
 }));
 
 // Rate Limiting
